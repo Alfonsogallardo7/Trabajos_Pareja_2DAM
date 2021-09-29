541b9c30a6dfd2bbcc3d5135009a80600f6f7e1e
javi navarro <tu@ejemplo.comlobosaur22@gmail.com>
2021-09-29 18:03:21 +0200

    añado el ejercicio 1 sin el dto

---
+++ DTO_Ejercicio_Clase_parte1/src/main/java/com/salesianostriana/dam/DTO_Ejercicio_Clase/model/AlumnoRepository.java
@@ -0,0 +1,7 @@
+package com.salesianostriana.dam.DTO_Ejercicio_Clase.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlumnoRepository extends JpaRepository<Alumno, Long> {
+
+}
---
+++ DTO_Ejercicio_Clase_parte1/src/main/java/com/salesianostriana/dam/DTO_Ejercicio_Clase/model/CursoRepository.java
@@ -0,0 +1,7 @@
+package com.salesianostriana.dam.DTO_Ejercicio_Clase.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CursoRepository extends JpaRepository<Curso, Long> {
+
+}
