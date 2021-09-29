78ed068fcb210440ff342d66f8e3569e04d29bb5
Alfonso Gallardo Rodríguez <gallardo.roalf20@triana.salesianos.edu>
2021-09-29 12:51:56 +0200

    Creando el proyecto

---
+++ DTO_Ejercicio_Clase/src/main/java/com/salesianostriana/dam/DTO_Ejercicio_Clase/DtoEjercicioClaseApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.DTO_Ejercicio_Clase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtoEjercicioClaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtoEjercicioClaseApplication.class, args);
+	}
+
+}
